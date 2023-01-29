c2d1f1027acf13afaaa5d23b815df52412047073
deepika030493 <123857598+deepika030493@users.noreply.github.com>
2023-01-29 16:52:37 +0530

    Login Page Feature

---
+++ DemoProject/src/myPackage/LoginPage.java
@@ -0,0 +1,10 @@
+package myPackage;
+
+public class LoginPage {
+
+	public static void main(String[] args) {
+		System.out.println("TaskCompleted");
+
+	}
+
+}
--- DemoProject/src/myPackage/MyClass.java
+++ DemoProject/src/myPackage/MyClass.java
@@ -1,5 +1,12 @@
 package myPackage;
 
 public class MyClass {
-
+	public static void main(String[]args)
+	{
+	System.out.println("TestBase");
+	System.out.println("pom.xml");
+	System.out.println("Utilities");
+	System.out.println("Resource");
+	System.out.println("Suite");
+}
 }
